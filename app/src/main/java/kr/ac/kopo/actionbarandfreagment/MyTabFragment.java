2e7f275689ab52a4fd981c643338dd726d479e9d
leehanseock <leehanseock@gmail.com>
2025-06-02 15:14:22 +0900

    7장 탭별로 이미지뷰 추가

--- app/src/main/java/kr/ac/kopo/actionbarandfreagment/MyTabFragment.java
+++ app/src/main/java/kr/ac/kopo/actionbarandfreagment/MyTabFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
@@ -29,14 +30,24 @@ public class MyTabFragment extends androidx.fragment.app.Fragment {
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         baseLayout.setLayoutParams(params);
 
-        if (tabName.equals("2024 1st Semster"))
-            baseLayout.setBackgroundColor(Color.rgb(232,206,194));
-        if (tabName.equals("2024 WCRC"))
-            baseLayout.setBackgroundColor(Color.rgb(217,229,255));
-        if (tabName.equals("2025 AI EXPO"))
-            baseLayout.setBackgroundColor(Color.rgb(206,251,201));
-        return baseLayout;
+        ImageView img1 = new ImageView(super.getActivity());
+        LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        img1.setImageResource(R.drawable.first);
+        img1.setAdjustViewBounds(true);
 
+        if (tabName.equals("2024 1st Semster")){
+            baseLayout.setBackgroundColor(Color.rgb(232,206,194));
+            img1.setImageResource(R.drawable.first);}
+        if (tabName.equals("2024 WCRC")){
+            baseLayout.setBackgroundColor(Color.rgb(217,229,255));
+            img1.setImageResource(R.drawable.wcrc);}
+        if (tabName.equals("2025 AI EXPO")){
+            baseLayout.setBackgroundColor(Color.rgb(206,251,201));
+            img1.setImageResource(R.drawable.aiexpo);}
+
+        baseLayout.addView(img1);
+
+        return baseLayout;
 //        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
